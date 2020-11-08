27da20595bdd51bab74295450b03716ac21d1b6b
HanNabu <uniondirfolder@gmail.com>
2020-11-08 23:51:26 +0200

    mod 3

---
+++ M3MAP1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace M3MAP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bn_Start = new System.Windows.Forms.Button();
+            this.tb_ResultGen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_LowBound = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_UpperBound = new System.Windows.Forms.TextBox();
+            this.bn_StartFib = new System.Windows.Forms.Button();
+            this.tb_FibMonitor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_FibNum = new System.Windows.Forms.TextBox();
+            this.bn_StopGenNum = new System.Windows.Forms.Button();
+            this.bt_StopCalc = new System.Windows.Forms.Button();
+            this.bn_Suspend = new System.Windows.Forms.Button();
+            this.bn_Proceed = new System.Windows.Forms.Button();
+            this.bt_Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bn_Start
+            // 
+            this.bn_Start.Location = new System.Drawing.Point(13, 13);
+            this.bn_Start.Name = "bn_Start";
+            this.bn_Start.Size = new System.Drawing.Size(173, 23);
+            this.bn_Start.TabIndex = 0;
+            this.bn_Start.Text = "Start";
+            this.bn_Start.UseVisualStyleBackColor = true;
+            this.bn_Start.Click += new System.EventHandler(this.bn_Start_Click);
+            // 
+            // tb_ResultGen
+            // 
+            this.tb_ResultGen.Location = new System.Drawing.Point(13, 95);
+            this.tb_ResultGen.Multiline = true;
+            this.tb_ResultGen.Name = "tb_ResultGen";
+            this.tb_ResultGen.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_ResultGen.Size = new System.Drawing.Size(172, 262);
+            this.tb_ResultGen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LowBound";
+            // 
+            // tb_LowBound
+            // 
+            this.tb_LowBound.Location = new System.Drawing.Point(85, 43);
+            this.tb_LowBound.Name = "tb_LowBound";
+            this.tb_LowBound.Size = new System.Drawing.Size(100, 20);
+            this.tb_LowBound.TabIndex = 3;
+            this.tb_LowBound.TextChanged += new System.EventHandler(this.tb_LowBound_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UpperBound";
+            // 
+            // tb_UpperBound
+            // 
+            this.tb_UpperBound.Location = new System.Drawing.Point(85, 69);
+            this.tb_UpperBound.Name = "tb_UpperBound";
+            this.tb_UpperBound.Size = new System.Drawing.Size(100, 20);
+            this.tb_UpperBound.TabIndex = 3;
+            this.tb_UpperBound.TextChanged += new System.EventHandler(this.tb_UpperBound_TextChanged);
+            // 
+            // bn_StartFib
+            // 
+            this.bn_StartFib.Location = new System.Drawing.Point(379, 12);
+            this.bn_StartFib.Name = "bn_StartFib";
+            this.bn_StartFib.Size = new System.Drawing.Size(173, 23);
+            this.bn_StartFib.TabIndex = 0;
+            this.bn_StartFib.Text = "Start";
+            this.bn_StartFib.UseVisualStyleBackColor = true;
+            this.bn_StartFib.Click += new System.EventHandler(this.bn_StartFib_Click);
+            // 
+            // tb_FibMonitor
+            // 
+            this.tb_FibMonitor.Location = new System.Drawing.Point(379, 94);
+            this.tb_FibMonitor.Multiline = true;
+            this.tb_FibMonitor.Name = "tb_FibMonitor";
+            this.tb_FibMonitor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_FibMonitor.Size = new System.Drawing.Size(172, 262);
+            this.tb_FibMonitor.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fib num->";
+            // 
+            // tb_FibNum
+            // 
+            this.tb_FibNum.Location = new System.Drawing.Point(451, 42);
+            this.tb_FibNum.Name = "tb_FibNum";
+            this.tb_FibNum.Size = new System.Drawing.Size(100, 20);
+            this.tb_FibNum.TabIndex = 3;
+            this.tb_FibNum.TextChanged += new System.EventHandler(this.tb_FibNum_TextChanged);
+            // 
+            // bn_StopGenNum
+            // 
+            this.bn_StopGenNum.Location = new System.Drawing.Point(192, 94);
+            this.bn_StopGenNum.Name = "bn_StopGenNum";
+            this.bn_StopGenNum.Size = new System.Drawing.Size(75, 23);
+            this.bn_StopGenNum.TabIndex = 4;
+            this.bn_StopGenNum.Text = "<- Stop";
+            this.bn_StopGenNum.UseVisualStyleBackColor = true;
+            this.bn_StopGenNum.Click += new System.EventHandler(this.bn_StopGenNum_Click);
+            // 
+            // bt_StopCalc
+            // 
+            this.bt_StopCalc.Location = new System.Drawing.Point(298, 95);
+            this.bt_StopCalc.Name = "bt_StopCalc";
+            this.bt_StopCalc.Size = new System.Drawing.Size(75, 23);
+            this.bt_StopCalc.TabIndex = 4;
+            this.bt_StopCalc.Text = "Stop ->";
+            this.bt_StopCalc.UseVisualStyleBackColor = true;
+            this.bt_StopCalc.Click += new System.EventHandler(this.bt_StopCalc_Click);
+            // 
+            // bn_Suspend
+            // 
+            this.bn_Suspend.Location = new System.Drawing.Point(298, 125);
+            this.bn_Suspend.Name = "bn_Suspend";
+            this.bn_Suspend.Size = new System.Drawing.Size(75, 23);
+            this.bn_Suspend.TabIndex = 5;
+            this.bn_Suspend.Text = "Suspend ->";
+            this.bn_Suspend.UseVisualStyleBackColor = true;
+            this.bn_Suspend.Click += new System.EventHandler(this.bn_Suspend_Click);
+            // 
+            // bn_Proceed
+            // 
+            this.bn_Proceed.Location = new System.Drawing.Point(298, 154);
+            this.bn_Proceed.Name = "bn_Proceed";
+            this.bn_Proceed.Size = new System.Drawing.Size(75, 23);
+            this.bn_Proceed.TabIndex = 5;
+            this.bn_Proceed.Text = "Resume ->";
+            this.bn_Proceed.UseVisualStyleBackColor = true;
+            this.bn_Proceed.Click += new System.EventHandler(this.bn_Proceed_Click);
+            // 
+            // bt_Restart
+            // 
+            this.bt_Restart.Location = new System.Drawing.Point(298, 183);
+            this.bt_Restart.Name = "bt_Restart";
+            this.bt_Restart.Size = new System.Drawing.Size(75, 23);
+            this.bt_Restart.TabIndex = 5;
+            this.bt_Restart.Text = "Restart ->";
+            this.bt_Restart.UseVisualStyleBackColor = true;
+            this.bt_Restart.Click += new System.EventHandler(this.bt_Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 399);
+            this.Controls.Add(this.bt_Restart);
+            this.Controls.Add(this.bn_Proceed);
+            this.Controls.Add(this.bn_Suspend);
+            this.Controls.Add(this.bt_StopCalc);
+            this.Controls.Add(this.bn_StopGenNum);
+            this.Controls.Add(this.tb_UpperBound);
+            this.Controls.Add(this.tb_FibNum);
+            this.Controls.Add(this.tb_LowBound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_FibMonitor);
+            this.Controls.Add(this.tb_ResultGen);
+            this.Controls.Add(this.bn_StartFib);
+            this.Controls.Add(this.bn_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bn_Start;
+        private System.Windows.Forms.TextBox tb_ResultGen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_LowBound;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_UpperBound;
+        private System.Windows.Forms.Button bn_StartFib;
+        private System.Windows.Forms.TextBox tb_FibMonitor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_FibNum;
+        private System.Windows.Forms.Button bn_StopGenNum;
+        private System.Windows.Forms.Button bt_StopCalc;
+        private System.Windows.Forms.Button bn_Suspend;
+        private System.Windows.Forms.Button bn_Proceed;
+        private System.Windows.Forms.Button bt_Restart;
+    }
+}
+
---
+++ M3MAP1/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThreadState = System.Threading.ThreadState;
+
+namespace M3MAP1
+{
+    public partial class Form1 : Form
+    {
+        private Thread Fibonacci;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bn_Suspend.Enabled = false;
+            bn_Proceed.Enabled = false;
+            bn_StopGenNum.Enabled = false;
+        }
+
+        private async void bn_Start_Click(object sender, EventArgs e)
+        {
+            bn_Start.Enabled = false;
+            tb_LowBound.Enabled = false;
+            tb_UpperBound.Enabled = false;
+
+            int min = 0; 
+            int max = 0;
+
+            if (tb_LowBound.Text.Length > 0)
+            {
+                min = int.Parse(tb_LowBound.Text);
+            }
+
+            if (tb_UpperBound.Text.Length > 0)
+            {
+                max= int.Parse(tb_UpperBound.Text);
+            }
+
+            await GenerateNumbers(min,max);
+        }
+
+        static readonly CancellationTokenSource CancelTokenSource = new CancellationTokenSource();
+        CancellationToken token = CancelTokenSource.Token;
+
+        static readonly CancellationTokenSource CancelTokenSource2 = new CancellationTokenSource();
+        CancellationToken token2 = CancelTokenSource.Token;
+
+        
+
+        private Task GenerateNumbers(int lowB, int uppB)
+        {
+            return Task.Run(() =>
+            {
+                
+                bool run = true;
+
+                string str;
+                while (run)
+                {
+                    
+                    if (uppB != 0)
+                        run = false;
+
+                    int i = lowB;
+
+                    if (lowB == 0)
+                    { i = 2;}
+
+                    int j = uppB;
+
+                    if (j == 0)
+                    { j = 10_000;}
+
+                    while (i != j)
+                    {
+                        if (token.IsCancellationRequested)
+                            return;
+                        str = "";
+                        int q = i + 10;
+                        while (i != q && i < j)
+                        {
+                            str += i.ToString()+",";
+                            ++i;
+                        }
+
+                        str += "\n\r";
+                        
+                        new Thread(() =>
+                        {
+
+                            Invoke(new Action(() => //UI Thread
+                                {
+                                    if (str != null) tb_ResultGen.Text += str;
+                                }
+
+                            ));
+
+                        }).Start();
+
+                        Thread.Sleep(1000);
+                    }
+                    
+                }
+                new Thread(() =>
+                {
+
+                    Invoke(new Action(() => //UI Thread
+                        {
+                            bn_Start.Enabled = true;
+                            tb_LowBound.Enabled = true;
+                            tb_UpperBound.Enabled = true;
+                        }
+                    ));
+
+                }).Start();
+            });
+            
+        }
+
+        private void tb_LowBound_TextChanged(object sender, EventArgs e)
+        {
+            if (tb_LowBound.Text.Length > 0)
+            {
+                try
+                {
+                   int test = int.Parse(tb_LowBound.Text);
+                   tb_LowBound.BackColor = Color.Bisque;
+                }
+                catch (Exception)
+                {
+                    tb_LowBound.BackColor = Color.Red;
+                    bn_Start.Enabled = false;
+                    return;
+                }
+            }
+            else
+            {
+                tb_LowBound.BackColor= Color.AliceBlue;
+            }
+            bn_Start.Enabled = true;
+        }
+
+        private void tb_UpperBound_TextChanged(object sender, EventArgs e)
+        {
+            if (tb_UpperBound.Text.Length > 0)
+            {
+                try
+                {
+                    int test = int.Parse(tb_UpperBound.Text);
+                    tb_UpperBound.BackColor = Color.Bisque;
+                }
+                catch (Exception)
+                {
+                    tb_UpperBound.BackColor = Color.Red;
+                    bn_Start.Enabled = false;
+                    return;
+                }
+            }
+            else
+            {
+                tb_UpperBound.BackColor = Color.AliceBlue;
+            }
+            bn_Start.Enabled = true;
+        }
+
+        private void bn_StopGenNum_Click(object sender, EventArgs e)
+        {
+            CancelTokenSource.Cancel();
+            bn_Start.Enabled = true;
+            tb_LowBound.Enabled = true;
+            tb_UpperBound.Enabled = true;
+            
+            
+        }
+
+        private void bt_StopCalc_Click(object sender, EventArgs e)
+        {
+            if (Fibonacci.IsAlive)
+            {
+                Fibonacci.Abort();
+            }
+            bn_Suspend.Enabled = false;
+        }
+
+        private void bn_Suspend_Click(object sender, EventArgs e)
+        {
+
+                Fibonacci.Suspend();
+                bn_Suspend.Enabled = false;
+                bn_Proceed.Enabled = true;
+ 
+        }
+
+        private void bn_Proceed_Click(object sender, EventArgs e)
+        {
+
+                Fibonacci.Resume();
+                bn_Suspend.Enabled = true;
+                bn_Proceed.Enabled = false;
+
+        }
+
+        private void bt_Restart_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(tb_FibNum.Text);
+            Fibonacci.Abort();
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(FibonacciTaskRun);
+            Fibonacci = new Thread(ts);
+            Fibonacci.Start((object)i);
+            
+        }
+
+        void FibonacciTaskRun(object a)
+        {
+
+            string str = string.Empty;
+
+            int n1 = 0, n2 = 1, n3, i, j=0, number=(int)a;
+            str += n1.ToString() + "\n\r" + n2.ToString() + "\n\r";
+
+            new Thread(() =>
+            {
+
+                Invoke(new Action(() => //UI Thread
+                    {
+                        tb_FibMonitor.Text += str;
+                    }
+
+                ));
+
+            }).Start();
+            for (i = 2; i < number; ++i)
+            {
+                str = "";
+                n3 = n1 + n2;
+                str += n3.ToString() + "\n\r";
+                n1 = n2;
+                n2 = n3;
+
+                new Thread(() =>
+                {
+
+                    Invoke(new Action(() => //UI Thread
+                        {
+                            tb_FibMonitor.Text += str;
+                        }
+
+                    ));
+
+                }).Start();
+                Thread.Sleep(500);
+            }
+        }
+
+
+        private void bn_StartFib_Click(object sender, EventArgs e)
+        {
+            int i = int.Parse(tb_FibNum.Text);
+            
+            ParameterizedThreadStart ts = new ParameterizedThreadStart(FibonacciTaskRun);
+            Fibonacci = new Thread(ts);
+            Fibonacci.Start((object)i);
+            bn_Suspend.Enabled = true;
+            bn_Proceed.Enabled =false;
+            bt_StopCalc.Enabled = true;
+        }
+
+        private void tb_FibNum_TextChanged(object sender, EventArgs e)
+        {
+            if (tb_UpperBound.Text.Length > 0)
+            {
+                try
+                {
+                    int test = int.Parse(tb_FibNum.Text);
+                    tb_UpperBound.BackColor = Color.Bisque;
+                }
+                catch (Exception)
+                {
+                    tb_FibNum.BackColor = Color.Red;
+                    bn_Start.Enabled = false;
+                    bt_Restart.Enabled = false;
+                    return;
+                }
+            }
+            else
+            {
+                tb_UpperBound.BackColor = Color.AliceBlue;
+            }
+            bn_Start.Enabled = true;
+            bt_Restart.Enabled = true;
+        }
+    }
+
+    
+}
